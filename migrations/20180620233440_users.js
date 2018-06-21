2a9cf6b8573511a10da537bc3c3435a04c9aecf4
Noel Escobedo <noel_ska15@hotmail.com>
2018-06-21 00:17:26 -0500

    Create a default user

--- migrations/20180620233440_users.js
+++ migrations/20180620233440_users.js
@@ -1,11 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  knex.schema
+  return knex.schema
     .createTable('users', function (t) {
         t.increments('id').primary();
         t.string('email', 40);
-        t.string('password_hash', 254);
-        t.string('full_name', 25);
+        t.string('password_hash', 254);;
+        t.string('full_name', 25);;
 
         t.timestamps();
     });
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var knexFile = require('../knexfile');
+var knex = require('knex')(knexFile[process.env.NODE_ENV || 'development']);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users/user');
 });
+
+router.post('/', function(req, res, next) {
+  knex('users').insert({
+    email: 'noel@michelada.io',
+    password_hash: '',
+    full_name: 'Noel Escobedo'
+  })
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.send('error');
+  })
+})
 
 module.exports = router;
