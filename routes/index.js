31c142d813a22d7a6a9288870bfd598ce495b81e
Noel Escobedo <noel@michelada.io>
2018-06-21 17:47:31 +0000

    Added id to user

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var knex = require('../knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  knex('users').select('email', 'full_name')
+  knex('users').select('id', 'email', 'full_name')
     .then(data => {
       res.render('index', { title: 'Mis usuarios', users: data });
     })
